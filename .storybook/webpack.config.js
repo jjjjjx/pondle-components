319b84a079e4f795518601a94cf9356fdc4edefb
James Lambert <jameslambert14@hotmail.com>
2017-09-13 14:46:39 +1000

    Extend storybook webpack config to use css modules

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
