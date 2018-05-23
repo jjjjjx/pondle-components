abdc9cf896232d42e886465a50dcdf40e6fe33a5
James Lambert <jameslambert14@hotmail.com>
2018-05-23 15:02:50 +1000

    Update click handler in Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,47 +4,57 @@ import styles from "./Button.css";
 import cx from "classnames";
 import classNamesBind from "classnames/bind";
 
-const Button = (props) => {
-  const { children, href, type, component, onClick, disabled, large, full, outlineOnDark, outlineOnLight, highlight, minimal, style, className, ...otherProps } = props;
-  let Element = href ? "a" : "button";
-  Element = component ? component : Element;
+class Button extends Component {
 
-  let moduleClassNames = classNamesBind.bind(styles);
+  handleClick() {
+    const { onClick, blurOnClick } = this.props;
+    if (blurOnClick) this.buttonDOM.blur();
+    if (onClick) this.props.onClick();
+  }
 
-  const classes = moduleClassNames({
-    button: true,
-    disabled: disabled,
-    large: large,
-    full: full,
-    outlineOnDark: outlineOnDark,
-    outlineOnLight: outlineOnLight,
-    highlight: highlight,
-    minimal: minimal
-  });
+  render() {
+    const { children, href, type, component, onClick, blurOnClick, disabled, large, full, outlineOnDark, outlineOnLight, highlight, minimal, style, className, ...otherProps } = this.props;
+    let Element = href ? "a" : "button";
+    Element = component ? component : Element;
 
-  const pndlClassNames = cx(
-    "pndl-button",
-    { "pndl-button--disabled": disabled },
-    { "pndl-button--large": large },
-    { "pndl-button--full": full },
-    { "pndl-button--outlineOnDark": outlineOnDark },
-    { "pndl-button--outlineOnLight": outlineOnLight },
-    { "pndl-button--highlight": highlight },
-    { "pndl-button--minimal": minimal }
-  );
+    let moduleClassNames = classNamesBind.bind(styles);
 
-  return (
-    <Element
-      className={cx(classes, className, pndlClassNames)}
-      style={style}
-      href={href}
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-      {...otherProps} >
-      {children}
-    </Element>
-  );
+    const classes = moduleClassNames({
+      button: true,
+      disabled: disabled,
+      large: large,
+      full: full,
+      outlineOnDark: outlineOnDark,
+      outlineOnLight: outlineOnLight,
+      highlight: highlight,
+      minimal: minimal
+    });
+
+    const pndlClassNames = cx(
+      "pndl-button",
+      { "pndl-button--disabled": disabled },
+      { "pndl-button--large": large },
+      { "pndl-button--full": full },
+      { "pndl-button--outlineOnDark": outlineOnDark },
+      { "pndl-button--outlineOnLight": outlineOnLight },
+      { "pndl-button--highlight": highlight },
+      { "pndl-button--minimal": minimal }
+    );
+
+    return (
+      <Element
+        className={cx(classes, className, pndlClassNames)}
+        style={style}
+        href={href}
+        type={type}
+        onClick={this.handleClick.bind(this)}
+        ref={buttonDOM => this.buttonDOM = buttonDOM}
+        disabled={disabled}
+        {...otherProps} >
+        {children}
+      </Element>
+    );
+  }
 };
 
 Button.propTypes = {
@@ -52,6 +62,7 @@ Button.propTypes = {
   type: PropTypes.string, // The button type, e.g. "submit"
   componet: PropTypes.element, // Use a component as the button element, e.g. a Link for react-router
   onClick: PropTypes.func, // Click event handler function
+  blurOnClick: PropTypes.bool, // If true the focus state will be removed from the button on click
   disabled: PropTypes.bool, // Disable this button
   large: PropTypes.bool, // Larger button size
   full: PropTypes.bool, // Button will span full width
